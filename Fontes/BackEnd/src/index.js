c85b37c83cdfb4e8d0ec86b3f83862c24ba4c944
eduardoportobaltazar <eduardo.porto.baltazar@gmail.com>
2022-03-03 21:48:21 -0300

    Atualizacao do Meddeare

--- Fontes/BackEnd/src/index.js
+++ Fontes/BackEnd/src/index.js
@@ -3,10 +3,14 @@ import { response } from 'express';
 
 const app = express ();
 
+//Middleware JSON
+app.use(express.json());
+
 
 app.get("/clientes", function(resquest, response){
     return response.send("Listando todos os dados...");        
 });
+
 
 app.listen(3000, function(){
   console.log('Servidor no Ar.')   
